da35a6f9b226c5caa4d933f43e84b458091f3f7a
Marei (peiTeX) <marei@peitex.de>
2022-10-05 11:22:16 +0200

    add bib to demofiles

--- build.lua
+++ build.lua
@@ -6,13 +6,15 @@ installfiles = {"tex/*.cls","*.cls"}
 sourcefiles  = installfiles
 unpackfiles  = { }
 
+packtdszip = true
+
 typesetexe="lualatex"
 typesetopts=""
 
 supportdir = "example/"
 
 typesetsuppfiles = {"*/*.tex", "*/*.bib"}
-demofiles = {"example/*/DEMO*.tex"}
+demofiles = {"example/*/*.tex", "example/*/*.bib"}
 typesetdemofiles = { "example/*/DEMO*.tex" }
 
 textfiles = {"*.md"}
