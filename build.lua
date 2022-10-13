68c399784a4703fa960bc16a962376cc9aec4e50
Marei (peiTeX) <marei@peitex.de>
2022-10-13 13:19:47 +0200

    add extra texlatex run to fix bib

--- build.lua
+++ build.lua
@@ -13,3 +13,25 @@ typesetdemofiles = {"DEMO*.tex"}
 typesetfiles = {"*.dtx", "DEMO*.tex"}
 
 textfiles = {"*.md"}
+
+function typeset(file,dir,exe)
+	dir = dir or "."
+	local errorlevel = tex(file,dir,exe)
+	if errorlevel ~= 0 then
+		return errorlevel
+	end
+	tex(file,dir,exe)-- add additional tex run to resolve issues concerning the bibliography
+	local name = jobname(file)
+	errorlevel = biber(name,dir) + bibtex(name,dir)
+	if errorlevel ~= 0 then
+		return errorlevel
+	end
+	for i = 2,typesetruns do
+		errorlevel =
+			makeindex(name,dir,".glo",".gls",".glg",glossarystyle) +
+			makeindex(name,dir,".idx",".ind",".ilg",indexstyle)    +
+			tex(file,dir,exe)
+		if errorlevel ~= 0 then break end
+	end
+	return errorlevel
+end
